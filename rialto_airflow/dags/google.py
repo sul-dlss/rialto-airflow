aba5d50324e92de1c681ca621d8ba8b9109a0686
Peter Mangiafico <peter@mangiafico.org>
2025-02-28 10:21:08 -0800

    add example of clearing a google sheet

--- rialto_airflow/dags/google.py
+++ rialto_airflow/dags/google.py
@@ -4,11 +4,7 @@ from airflow.decorators import dag, task
 from airflow.providers.google.suite.transfers.local_to_drive import (
     LocalFilesystemToGoogleDriveOperator,
 )
-from airflow.providers.google.suite.operators.sheets import (
-    GoogleSheetsCreateSpreadsheetOperator,
-)
 from airflow.providers.google.suite.hooks.sheets import GSheetsHook
-
 
 @dag(
     # schedule=@weekly,
@@ -17,47 +13,28 @@ from airflow.providers.google.suite.hooks.sheets import GSheetsHook
 )
 def google():
     @task
-    def publish_file_to_google_drive():
+    def publish_file_to_google_drive(local_filename, drive_folder_id):
         """
-        Publish a file to Google Drive.
+        Publish a file to a Google Drive folder.
         """
-        filename = "/opt/airflow/rialto_airflow/dags/google.py"
-        drive_folder_id = "1xjxrCUrA0yrOt0i5wNTrWB-841GJ_VDL"
         LocalFilesystemToGoogleDriveOperator(
             gcp_conn_id="google_cloud_default",
             task_id="upload_to_drive",
-            local_paths=[filename],
+            local_paths=[local_filename],
             folder_id=drive_folder_id,
             drive_folder="",
         ).execute(context={})
 
     @task
-    def create_google_sheet():
+    def clear_google_sheet(spreadsheet_id):
         """
-        Create a Google Sheet.
+        Clear the contents of an existing Google Sheet.
         """
-        spreadsheet = {
-            "properties": {
-                "title": "ORCID Integration Stats",
-            },
-            "sheets": [
-                {
-                    "properties": {
-                        "title": "Sheet1",
-                    },
-                },
-            ],
-        }
-        GoogleSheetsCreateSpreadsheetOperator(
-            task_id="create_spreadsheet",
-            spreadsheet=spreadsheet,
-            gcp_conn_id="google_cloud_default",
-        ).execute(context={})
-        # print_spreadsheet_url = BashOperator(
-        #     task_id="print_spreadsheet_url",
-        #     bash_command=f"echo {XComArg(create_spreadsheet, key='spreadsheet_url')}",
-        # )
-        # print("Spreadsheet URL: ", print_spreadsheet_url)
+        hook = GSheetsHook(gcp_conn_id="google_cloud_default")
+        hook.clear(
+            spreadsheet_id=spreadsheet_id,
+            range_="Sheet1",
+        )
 
     @task
     def append_rows_to_google_sheet(spreadsheet_id, values):
@@ -67,18 +44,19 @@ def google():
         hook = GSheetsHook(gcp_conn_id="google_cloud_default")
         hook.append_values(
             spreadsheet_id=spreadsheet_id,
-            range_="Sheet1!A1:B1",
+            range_="Sheet1",
             values=values,
             value_input_option="RAW",
         )
 
-    publish_file_to_google_drive()
+    publish_file_to_google_drive("/opt/airflow/rialto_airflow/dags/google.py", "1xjxrCUrA0yrOt0i5wNTrWB-841GJ_VDL")
 
-    create_google_sheet()
+    clear_google_sheet(
+        "1FKyGKzRu2M7Swd8pPCXG2Z2taiCtNkEFhwcIMJIPfi8")
 
     append_rows_to_google_sheet(
-        "1FKyGKzRu2M7Swd8pPCXG2Z2taiCtNkEFhwcIMJIPfi8", [["Hello", "World"]]
-    )
+        "1FKyGKzRu2M7Swd8pPCXG2Z2taiCtNkEFhwcIMJIPfi8",
+        [["Hello", "World", datetime.date.today().isoformat(), datetime.datetime.now().isoformat()]])
 
 
 google()

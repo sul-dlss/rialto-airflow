d32f9aa16193a6783db4c85c687a0f825d782b89
Peter Mangiafico <peter@mangiafico.org>
2025-03-03 11:57:05 -0800

    additional examples of google integration; updates to config

--- rialto_airflow/dags/google.py
+++ rialto_airflow/dags/google.py
@@ -1,10 +1,21 @@
+# An example DAG showing how to interact with Google Sheets and Google Drive using Airflow.
+# Pull these examples into your own DAGs as needed
+
 import datetime
 
 from airflow.decorators import dag, task
+from airflow.models import Variable
 from airflow.providers.google.suite.transfers.local_to_drive import (
     LocalFilesystemToGoogleDriveOperator,
 )
 from airflow.providers.google.suite.hooks.sheets import GSheetsHook
+
+gcp_conn_id = Variable.get("google_connection")
+google_drive_id = Variable.get("google_drive_id")
+google_sheet_id = Variable.get(
+    "orcid_stats_sheet_id"
+)  # setup in Airflow Admin -> Variables OR via vault/puppet environment variables
+
 
 @dag(
     # schedule=@weekly,
@@ -13,25 +24,11 @@ from airflow.providers.google.suite.hooks.sheets import GSheetsHook
 )
 def google():
     @task
-    def publish_file_to_google_drive(local_filename, drive_folder_id):
-        """
-        Publish a file to a Google Drive folder.
-        """
-        LocalFilesystemToGoogleDriveOperator(
-            gcp_conn_id="google_cloud_default",
-            task_id="upload_to_drive",
-            local_paths=[local_filename],
-            folder_id=drive_folder_id,
-            drive_folder="",
-        ).execute(context={})
-
-    @task
     def clear_google_sheet(spreadsheet_id):
         """
         Clear the contents of an existing Google Sheet.
         """
-        hook = GSheetsHook(gcp_conn_id="google_cloud_default")
-        hook.clear(
+        GSheetsHook(gcp_conn_id=gcp_conn_id).clear(
             spreadsheet_id=spreadsheet_id,
             range_="Sheet1",
         )
@@ -41,22 +38,37 @@ def google():
         """
         Append rows to an existing Google Sheet.
         """
-        hook = GSheetsHook(gcp_conn_id="google_cloud_default")
-        hook.append_values(
+        GSheetsHook(gcp_conn_id=gcp_conn_id).append_values(
             spreadsheet_id=spreadsheet_id,
             range_="Sheet1",
             values=values,
             value_input_option="RAW",
         )
 
-    publish_file_to_google_drive("/opt/airflow/rialto_airflow/dags/google.py", "1xjxrCUrA0yrOt0i5wNTrWB-841GJ_VDL")
+    # upload a file to Google Drive ("task_id" will be the name of the task in the Airflow UI)
+    # if you need to setup dependencies, you can assign the return result to a variable, which is then
+    # referenced in future steps, using the >> syntax
+    LocalFilesystemToGoogleDriveOperator(
+        gcp_conn_id=gcp_conn_id,
+        task_id="upload_fie_to_drive",
+        local_paths=["/opt/airflow/rialto_airflow/dags/harvest.py"],
+        folder_id=google_drive_id,
+        drive_folder="",
+    )
 
-    clear_google_sheet(
-        "1FKyGKzRu2M7Swd8pPCXG2Z2taiCtNkEFhwcIMJIPfi8")
+    sheet_cleared = clear_google_sheet(google_sheet_id)
 
-    append_rows_to_google_sheet(
-        "1FKyGKzRu2M7Swd8pPCXG2Z2taiCtNkEFhwcIMJIPfi8",
-        [["Hello", "World", datetime.date.today().isoformat(), datetime.datetime.now().isoformat()]])
+    sheet_cleared >> append_rows_to_google_sheet(
+        google_sheet_id,
+        [
+            [
+                "Hello",
+                "World",
+                datetime.date.today().isoformat(),
+                datetime.datetime.now().isoformat(),
+            ]
+        ],
+    )
 
 
 google()

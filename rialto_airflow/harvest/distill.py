866962829631c2e699d323e311f3664ea7cb68e5
Peter Mangiafico <peter@mangiafico.org>
2025-05-02 14:06:56 -0700

    adjust pub year order to put sulpub last

--- rialto_airflow/harvest/distill.py
+++ rialto_airflow/harvest/distill.py
@@ -77,12 +77,12 @@ def _pub_year(pub):
     return _first(
         pub,
         rules=[
-            JsonPathRule("sulpub_json", "year", is_valid_year=True),
             JsonPathRule("dim_json", "year", is_valid_year=True),
             JsonPathRule("openalex_json", "publication_year", is_valid_year=True),
             JsonPathRule(
                 "wos_json", "static_data.summary.pub_info.pubyear", is_valid_year=True
             ),
+            JsonPathRule("sulpub_json", "year", is_valid_year=True),
         ],
     )
 

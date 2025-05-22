f9fdf21c2440ab92f5b8cca6bef70797d5f7491f
Peter Mangiafico <peter@mangiafico.org>
2025-05-22 15:18:06 -0700

    properly export contributions grouped by school and school/department

--- test/publish/test_publication.py
+++ test/publish/test_publication.py
@@ -52,67 +52,66 @@ def dataset(test_session):
             academic_council=True,
         )
 
+        author2 = Author(
+            first_name="Leland",
+            last_name="Stanford",
+            sunet="lelands",
+            cap_profile_id="12345",
+            orcid="02980983434",
+            primary_school="School of Humanities and Sciences",
+            primary_dept="Social Sciences",
+            primary_role="staff",
+            schools=[
+                "School of Humanities and Sciences",
+            ],
+            departments=["Social Sciences"],
+            academic_council=False,
+        )
+
+        author3 = Author(
+            first_name="Frederick",
+            last_name="Olmstead",
+            sunet="folms",
+            cap_profile_id="123456",
+            orcid="02980983422",
+            primary_school="School of Engineering",
+            primary_dept="Mechanical Engineering",
+            primary_role="faculty",
+            schools=["School of Engineering"],
+            departments=["Mechanical Engineering"],
+            academic_council=False,
+        )
+
+        author4 = Author(
+            first_name="Frederick",
+            last_name="Terman",
+            sunet="fterm",
+            cap_profile_id="1234567",
+            orcid="029809834222",
+            primary_school="School of Engineering",
+            primary_dept="Electrical Engineering",
+            primary_role="faculty",
+            schools=["School of Engineering"],
+            departments=["Electrical Engineering"],
+            academic_council=True,
+        )
+
+        funder1 = Funder(
+            name="National Institutes of Health", grid_id="12345", federal=True
+        )
+        funder2 = Funder(
+            name="Andrew Mellon Foundation", grid_id="123456", federal=False
+        )
+
         pub.authors.append(author1)
-
-        pub.authors.append(
-            Author(
-                first_name="Leland",
-                last_name="Stanford",
-                sunet="lelands",
-                cap_profile_id="12345",
-                orcid="02980983434",
-                primary_school="School of Humanities and Sciences",
-                primary_dept="Social Sciences",
-                primary_role="staff",
-                schools=[
-                    "School of Humanities and Sciences",
-                ],
-                departments=["Social Sciences"],
-                academic_council=False,
-            )
-        )
-
-        pub.authors.append(
-            Author(
-                first_name="Frederick",
-                last_name="Olmstead",
-                sunet="folms",
-                cap_profile_id="123456",
-                orcid="02980983422",
-                primary_school="School of Engineering",
-                primary_dept="Mechanical Engineering",
-                primary_role="faculty",
-                schools=["School of Engineering"],
-                departments=["Mechanical Engineering"],
-                academic_council=False,
-            )
-        )
-
-        pub.authors.append(
-            Author(
-                first_name="Frederick",
-                last_name="Terman",
-                sunet="fterm",
-                cap_profile_id="1234567",
-                orcid="029809834222",
-                primary_school="School of Engineering",
-                primary_dept="Electrical Engineering",
-                primary_role="faculty",
-                schools=["School of Engineering"],
-                departments=["Electrical Engineering"],
-                academic_council=True,
-            )
-        )
+        pub.authors.append(author2)
+        pub.authors.append(author3)
+        pub.authors.append(author4)
+        pub.funders.append(funder1)
+        pub.funders.append(funder2)
 
         pub2.authors.append(author1)
-
-        pub.funders.append(
-            Funder(name="National Institutes of Health", grid_id="12345", federal=True)
-        )
-
-        pub.funders.append(
-            Funder(name="Andrew Mellon Foundation", grid_id="123456", federal=False)
-        )
+        pub2.funders.append(funder1)
 
         session.add(pub)
         session.add(pub2)
@@ -138,7 +137,9 @@ def test_write_publications(test_session, snapshot, dataset, caplog):
 
     # read it in and make sure it looks right
     df = pandas.read_csv(csv_path)
+    # There are two rows in the dataset because there are two publications
     assert len(df) == 2
+
     row = df.iloc[0]
     assert row.doi == "10.000/000001"
     assert row.pub_year == 2023
@@ -169,11 +170,17 @@ def test_write_contributions_by_school(test_session, snapshot, dataset, caplog):
 
     # read it in and make sure it looks right
     df = pandas.read_csv(csv_path)
+    # There are three rows in the dataset. We have two publications and four authors total.
+    # The first publication has four authors, and the second publication has one author (one of the same authors as the first publication).
+    # Two of the authors are in the same school, and two are in different schools.
+    # So the first publication will have two rows, one for each school for which there is an author (two others grouped together for the school)
+    # The second publication will have one row (for the school of the single author).
     assert len(df) == 3
 
     # sort it so we know what is in each row
     df = df.sort_values(["doi"])
 
+    # Rows 1 and 2 are the same publication, but different schools
     row = df.iloc[0]
     assert bool(row.academic_council_authored) is True
     assert row.journal == "Delicious Limes Journal of Science"
@@ -189,10 +196,30 @@ def test_write_contributions_by_school(test_session, snapshot, dataset, caplog):
     assert bool(row.faculty_authored) is True
     assert bool(row.federally_funded) is True
     assert row.open_access == "gold"
-    assert row.primary_school == "School of Humanities and Sciences"
+    assert row.primary_school == "School of Humanities and Sciences"  # school 1
     assert row.pub_year == 2023
     assert row.types == "article|preprint"
 
+    row = df.iloc[1]
+    assert bool(row.academic_council_authored) is True
+    assert row.journal == "Delicious Limes Journal of Science"
+    assert row.issue == 12
+    assert row.pages == "1-10"
+    assert row.volume == 1
+    assert row.pmid == 36857419
+    assert row.mesh == "Delicions|Limes"
+    assert row.url == "https://example_dim.com"
+    assert row.title == "My Life"
+    assert row.apc == 123
+    assert row.doi == "10.000/000001"
+    assert bool(row.faculty_authored) is True
+    assert bool(row.federally_funded) is True
+    assert row.open_access == "gold"
+    assert row.primary_school == "School of Engineering"  # school 2
+    assert row.pub_year == 2023
+    assert row.types == "article|preprint"
+
+    # Row 3 is the second publication
     row = df.iloc[2]
     assert bool(row.academic_council_authored) is True
     assert row.journal == "Delicious Limes Journal of Science"
@@ -222,11 +249,19 @@ def test_write_contributions_by_department(test_session, snapshot, dataset, capl
 
     # read it in and make sure it looks right
     df = pandas.read_csv(csv_path)
+    # There are four rows in the dataset. We have two publications and four authors total.
+    # The first publication has four authors, and the second publication has one author (one of the same authors as the first publication).
+    # Two of the authors are in the same school, and two are in different schools.
+    # Two of the authors in the same school are also in the same department, but the two in the other school are in different departments.
+    # So the first publication will have three rows, one for the authors which share the same school AND deparment,
+    # and two for the authors which share the same school BUT not the same department.
+    # The second publication will have one row (for the school and department of the single author).
     assert len(df) == 4
 
     # sort it so we know what is in each row
     df = df.sort_values(["doi"])
 
+    # Rows 1-3 are the same publication, but different department/school combinations
     row = df.iloc[0]
     assert bool(row.academic_council_authored) is True
     assert row.journal == "Delicious Limes Journal of Science"
@@ -242,7 +277,9 @@ def test_write_contributions_by_department(test_session, snapshot, dataset, capl
     assert bool(row.faculty_authored) is True
     assert bool(row.federally_funded) is True
     assert row.open_access == "gold"
-    assert row.primary_school == "School of Humanities and Sciences"
+    assert (
+        row.primary_school == "School of Humanities and Sciences"
+    )  # two authors with same school AND same department, one row
     assert row.primary_department == "Social Sciences"
     assert row.pub_year == 2023
     assert row.types == "article|preprint"
@@ -262,7 +299,9 @@ def test_write_contributions_by_department(test_session, snapshot, dataset, capl
     assert bool(row.faculty_authored) is True
     assert bool(row.federally_funded) is True
     assert row.open_access == "gold"
-    assert row.primary_school == "School of Engineering"
+    assert (
+        row.primary_school == "School of Engineering"
+    )  # one author with same school BUT different department
     assert row.primary_department == "Mechanical Engineering"
     assert row.pub_year == 2023
     assert row.types == "article|preprint"
@@ -282,11 +321,14 @@ def test_write_contributions_by_department(test_session, snapshot, dataset, capl
     assert bool(row.faculty_authored) is True
     assert bool(row.federally_funded) is True
     assert row.open_access == "gold"
-    assert row.primary_school == "School of Engineering"
+    assert (
+        row.primary_school == "School of Engineering"
+    )  # other author with same school BUT different department
     assert row.primary_department == "Electrical Engineering"
     assert row.pub_year == 2023
     assert row.types == "article|preprint"
 
+    # Row 4 is the second publication
     row = df.iloc[3]
     assert bool(row.academic_council_authored) is True
     assert row.journal == "Delicious Limes Journal of Science"

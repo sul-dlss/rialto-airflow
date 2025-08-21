a2f49f39c60169104a054b701ae3f8ef5cf15e94
Laura Wrubel <lwrubel@stanford.edu>
2025-08-21 16:14:05 -0400

    Deduplication for a post-process step

---
+++ rialto_airflow/harvest/deduplicate.py
@@ -0,0 +1,55 @@
+import logging
+from sqlalchemy import delete, func, insert, select
+
+from rialto_airflow.database import Publication, get_session, pub_author_association
+from rialto_airflow.snapshot import Snapshot
+
+
+def remove_duplicates(snapshot: Snapshot) -> int:
+    logging.info("Removing any duplicate publications.")
+    with get_session(snapshot.database_name).begin() as session:
+        # Find all duplicate WOS publications in the snapshot
+        duplicates = session.execute(
+            select(func.count(), Publication.wos_json["UID"])
+            .where(Publication.doi.is_(None))
+            .where(Publication.wos_json["UID"].is_not(None))
+            .group_by(Publication.wos_json["UID"])
+            .having(func.count() > 1)
+        ).all()
+        num_dupes = len(duplicates)
+        logging.info(f"Found {num_dupes} publication(s) with duplicates.")
+
+        wos_uids = [row[1] for row in duplicates]
+        for wos_uid in wos_uids:
+            pubs = (
+                session.execute(
+                    select(Publication).where(
+                        Publication.wos_json["UID"].astext == wos_uid
+                    )
+                )
+                .scalars()
+                .all()
+            )
+            # pubs contains all Publications with this WOS UID
+            # keep the first one and merge authors
+            main_pub = pubs[0].id
+            logging.info(
+                f"Keeping publication {main_pub} as the record for this publication."
+            )
+            for pub in pubs[1:]:
+                # Move author relationships to the first instance
+                for author in pub.authors:
+                    session.execute(
+                        insert(pub_author_association).values(
+                            publication_id=main_pub, author_id=author.id
+                        )
+                    )
+                    session.execute(
+                        delete(pub_author_association)
+                        .where(pub_author_association.c.publication_id == pub.id)
+                        .where(pub_author_association.c.author_id == author.id)
+                    )
+                # Remove the duplicate publication
+                # This could strand an Author with no publications?
+                session.execute(delete(Publication).where(Publication.id == pub.id))
+    return num_dupes
---
+++ test/harvest/test_deduplicate.py
@@ -0,0 +1,92 @@
+import pytest
+
+from rialto_airflow.harvest import deduplicate
+import test.publish.data as test_data
+from rialto_airflow.database import Author, Publication
+
+
+@pytest.fixture
+def dataset(test_session):
+    """
+    This fixture will create two publications that are duplicates and lack DOIs.
+    """
+    with test_session.begin() as session:
+        pub = Publication(
+            doi=None,
+            title="My Life",
+            apc=123,
+            open_access="green",
+            pub_year=2024,
+            dim_json=test_data.dim_json(),
+            openalex_json=test_data.openalex_json(),
+            wos_json=test_data.wos_json(),
+            sulpub_json=test_data.sulpub_json(),
+            pubmed_json=test_data.pubmed_json(),
+        )
+
+        pub2 = Publication(
+            doi=None,
+            title="My Life",
+            apc=123,
+            open_access="green",
+            pub_year=2024,
+            dim_json=test_data.dim_json(),
+            openalex_json=test_data.openalex_json(),
+            wos_json=test_data.wos_json(),
+            sulpub_json=test_data.sulpub_json(),
+            pubmed_json=test_data.pubmed_json(),
+        )
+
+        author1 = Author(
+            first_name="Jane",
+            last_name="Stanford",
+            sunet="janes",
+            cap_profile_id="1234",
+            orcid="0298098343",
+            primary_school="School of Humanities and Sciences",
+            primary_dept="Social Sciences",
+            primary_role="faculty",
+            schools=[
+                "Vice Provost for Undergraduate Education",
+                "School of Humanities and Sciences",
+            ],
+            departments=["Inter-Departmental Programs", "Social Sciences"],
+            academic_council=True,
+        )
+
+        author2 = Author(
+            first_name="Leland",
+            last_name="Stanford",
+            sunet="lelands",
+            cap_profile_id="12345",
+            orcid="02980983434",
+            primary_school="School of Humanities and Sciences",
+            primary_dept="Social Sciences",
+            primary_role="staff",
+            schools=[
+                "School of Humanities and Sciences",
+            ],
+            departments=["Social Sciences"],
+            academic_council=False,
+        )
+
+        pub.authors.append(author1)
+        pub2.authors.append(author2)
+        session.add(pub)
+        session.add(pub2)
+
+
+def test_wos_deduplicate(test_session, dataset, snapshot):
+    """
+    Test that the publication with a duplicate is found and the duplicates removed.
+    Authors should be moved to the remaining record.
+    """
+    dupes = deduplicate.remove_duplicates(snapshot)
+    assert dupes == 1
+    with test_session.begin() as session:
+        assert session.query(Publication).count() == 1
+        pubs = session.query(Publication).where(
+            Publication.wos_json["UID"].astext == "WOS:000123456789"
+        )
+        assert pubs.count() == 1
+        assert len(pubs.one().authors) == 2

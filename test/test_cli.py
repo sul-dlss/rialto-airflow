47b923d6c9e69d7cf6ba3d7f17a9fbf6945f2721
Ed Summers <ehs@pobox.com>
2025-12-17 14:57:42 -0500

    cli can use remote db
    This commit adds:
    
    - a --db-uri option to connect to a remote database
    - a `snapshots` command to list snapshot databases available
    - output platform identifiers for tracking down data if necessary

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,8 +1,6 @@
-import os
 from io import StringIO
 
 import pandas
-import pytest
 from typer.testing import CliRunner
 
 from rialto_airflow.cli import app
@@ -16,19 +14,17 @@ def test_publications(test_session, snapshot, dataset):
 
     df = pandas.read_csv(StringIO(result.output))
     assert len(df) == 2
-    assert len(df.columns) == 10
+    assert len(df.columns) == 15
 
     row = df.iloc[0]
     assert row.doi == "10.000/000001"
     assert row.title == "My Life"
     assert row.pub_year == 2023
-    assert row.sources == "sulpub|crossref|dim|wos|openalex|pubmed"
 
     row = df.iloc[1]
     assert row.doi == "10.000/000002"
     assert row.title == "My Life Part 2"
     assert row.pub_year == 2024
-    assert row.sources == "sulpub|dim|wos|pubmed"
 
 
 def test_publications_no_author(test_session, snapshot, dataset):
@@ -37,21 +33,6 @@ def test_publications_no_author(test_session, snapshot, dataset):
     )
     assert result.exit_code == 1
     assert result.output.strip() == "The author fiddlesticks does not exist"
-
-
-@pytest.fixture()
-def empty_data_dir(tmp_path):
-    os.environ["AIRFLOW_VAR_DATA_DIR"] = str(tmp_path)
-
-
-def test_publications_no_db_name(empty_data_dir):
-    result = runner.invoke(app, ["publications", "janes"])
-    assert result.exit_code == 1
-
-
-def test_authors_no_db_name(empty_data_dir):
-    result = runner.invoke(app, ["authors"])
-    assert result.exit_code == 1
 
 
 def test_authors(test_session, snapshot, dataset):
